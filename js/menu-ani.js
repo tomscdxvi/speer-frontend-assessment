4b0f350fc5efe2a36ae9f8b4826a1d06e0a45b81
Tommy Chiu <toms.cdxvi@gmail.com>
2021-05-08 02:16:32 -0400

    Initial Commit

---
+++ js/menu-ani.js
@@ -0,0 +1,9 @@
+function toggleMenu()
+{
+    let menu = document.querySelector(".menu");
+    menu.classList.toggle("toggleClass");
+}
+
+let hamburger = documnet.querySelector(".McButton");
+
+hamburger.addEventListener("click", toggleClass)
---
+++ js/menu.js
@@ -0,0 +1,26 @@
+var hamBtn = $("[data=hamburger-menu]");
+var Bar1 = hamBtn.find("b:nth-child(1)");
+var Bar2 = hamBtn.find("b:nth-child(2)");
+var Bar3 = hamBtn.find("b:nth-child(3)");
+
+
+
+hamBtn.click(function() 
+{
+    $(this).toggleClass("active");
+
+    if (hamBtn.hasClass("active")) 
+    {
+        Bar1.velocity({ top: "50%" }, {duration: 200, easing: "swing"});
+        Bar3.velocity({ top: "50%" }, {duration: 200, easing: "swing"})
+                .velocity({rotateZ:"90deg"}, {duration: 800, delay: 200, easing: [500,20] });
+        hamBtn.velocity({rotateZ:"135deg"}, {duration: 800, delay: 200, easing: [500,20] });
+    } 
+    else 
+    {
+        hamBtn.velocity("reverse");
+        Bar3.velocity({rotateZ:"0deg"}, {duration: 800, easing: [500,20] })
+                .velocity({ top: "100%" }, {duration: 200, easing: "swing"});
+        Bar1.velocity("reverse", {delay: 800});
+    }
+});
