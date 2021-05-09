6a0d5e566e27b324bc81a02ddf467bb7be3e1391
Tommy Chiu <toms.cdxvi@gmail.com>
2021-05-08 22:39:04 -0400

    Pricing Page Done

--- js/btn.js
+++ js/btn.js
@@ -1,4 +1,5 @@
-document.querySelector('.button1').onmousemove = function (e) {
+document.querySelector('.button1').onmousemove = function(e) 
+{
 
 	var x = e.pageX - e.target.offsetLeft;
 	var y = e.pageY - e.target.offsetTop;
@@ -7,11 +8,107 @@ document.querySelector('.button1').onmousemove = function (e) {
 	e.target.style.setProperty('--y', y + 'px');
 };
 
-document.querySelector('.button2').onmousemove = function (e) {
+document.querySelector('.button2').onmousemove = function(e) 
+{
 
 	var x = e.pageX - e.target.offsetLeft;
 	var y = e.pageY - e.target.offsetTop;
 
 	e.target.style.setProperty('--x', x + 'px');
 	e.target.style.setProperty('--y', y + 'px');
+};
+
+document.querySelector('.button3').onmousemove = function(e) 
+{
+
+	var x = e.pageX - e.target.offsetLeft;
+	var y = e.pageY - e.target.offsetTop;
+
+	e.target.style.setProperty('--x', x + 'px');
+	e.target.style.setProperty('--y', y + 'px');
+};
+
+document.querySelector('.button4').onmousemove = function(e) 
+{
+
+	var x = e.pageX - e.target.offsetLeft;
+	var y = e.pageY - e.target.offsetTop;
+
+	e.target.style.setProperty('--x', x + 'px');
+	e.target.style.setProperty('--y', y + 'px');
+};
+
+document.querySelector('.button5').onmousemove = function(e) 
+{
+
+	var x = e.pageX - e.target.offsetLeft;
+	var y = e.pageY - e.target.offsetTop;
+
+	e.target.style.setProperty('--x', x + 'px');
+	e.target.style.setProperty('--y', y + 'px');
+};
+
+document.querySelector('.button6').onmousemove = function(e) 
+{
+
+	var x = e.pageX - e.target.offsetLeft;
+	var y = e.pageY - e.target.offsetTop;
+
+	e.target.style.setProperty('--x', x + 'px');
+	e.target.style.setProperty('--y', y + 'px');
+};
+
+document.querySelector('.pricing-btn-1').onmousemove = function(e) 
+{
+
+	var x = e.pageX - e.target.offsetLeft;
+	var y = e.pageY - e.target.offsetTop;
+
+	e.target.style.setProperty('--x', x + 'px');
+	e.target.style.setProperty('--y', y + 'px');
+};
+
+document.querySelector('.pricing-btn-2').onmousemove = function(e) 
+{
+
+	var x = e.pageX - e.target.offsetLeft;
+	var y = e.pageY - e.target.offsetTop;
+
+	e.target.style.setProperty('--x', x + 'px');
+	e.target.style.setProperty('--y', y + 'px');
+};
+
+document.querySelector('.pricing-btn-3').onmousemove = function(e) 
+{
+
+	var x = e.pageX - e.target.offsetLeft;
+	var y = e.pageY - e.target.offsetTop;
+
+	e.target.style.setProperty('--x', x + 'px');
+	e.target.style.setProperty('--y', y + 'px');
+};
+
+document.getElementById("button1").onclick = function() 
+{
+        location.href = "pricing.html";
+};
+
+document.getElementById("button2").onclick = function() 
+{
+        location.href = "pricing.html";
+};
+
+document.getElementById("button3").onclick = function() 
+{
+        location.href = "pricing.html";
+};
+
+document.getElementById("button4").onclick = function() 
+{
+        location.href = "pricing.html";
+};
+
+document.getElementById("button5").onclick = function() 
+{
+        location.href = "pricing.html";
 };
